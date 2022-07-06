e223551d7c10ebfc40fcaf2833d5c6d780065fe7
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2022-07-06 11:36:33 +0300

    test

--- main.js
+++ main.js
@@ -1,19 +1,41 @@
-const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require("node-telegram-bot-api");
 
-const token = process.argv[2];
+const ids = [1377576664, 947163681,]; //838805281, 734752306];
 
-const bot = new TelegramBot(token, {webHook: { port: process.env.PORT }});
-const url = process.env.APP_URL || 'https://muver-ping-bot.herokuapp.com:443';
+const token = "5546984481:AAE7lWvJP9xTCYSng3EYgkGyZB1nZWA0D0E";
+// const options = {
+//   polling: true,
+// };
+const options = {
+  webHook: {
+    port: process.env.PORT,
+  },
+};
+
+const bot = new TelegramBot(token, options);
+
+const url = process.env.APP_URL || 'https://muver-ping-bot.herokuapp.com:443';;
 bot.setWebHook(`${url}/bot${token}`);
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
+bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
-  const resp = match[1];
-
-  bot.sendMessage(chatId, resp);
-});
-
-// Just to ping!
-bot.on('message', function onMessage(msg) {
-    bot.sendMessage(msg.chat.id, 'I am alive on Heroku!');
+  try {
+    if (msg.text !== undefined) {
+      if (msg.text.includes("@all")) {
+        let message = "";
+        for (const id of ids) {
+          if (id !== msg.from.id) {
+            const member = await bot.getChatMember(chatId, id);
+            const name = member.user.first_name;
+            message += `[${name}](tg://user?id=${id}) `;
+          }
+        }
+        bot.sendMessage(chatId, message, { parse_mode: 'MarkdownV2' });
+      }
+    } else {
+      bot.sendMessage(chatId, `all not found`);
+    }
+  } catch (e) {
+    bot.sendMessage(chatId, `Something went wrong: ${e}`);
+  }
 });
