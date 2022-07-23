932af7fa074b862d8af9f902cc4d7f7f62dea760
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2022-07-23 11:08:20 +0300

    add some logic

--- main.js
+++ main.js
@@ -1,40 +1,44 @@
 const TelegramBot = require("node-telegram-bot-api");
 
-const ids = [1377576664, 947163681, 838805281, 734752306];
-
-const token = process.env.TOKEN;
-// const options = {
-//   polling: true,
-// };
-const options = {
+const IDS = [1377576664, 947163681, 838805281, 734752306];
+const TOKEN = process.env.TOKEN;
+const OPTIONS = {
   webHook: {
     port: process.env.PORT,
   },
 };
 
-const bot = new TelegramBot(token, options);
-console.log("bot online");
-const url = process.env.APP_URL || 'https://floating-falls-26181.herokuapp.com:443';;
-bot.setWebHook(`${url}/bot${token}`);
-console.log(`webhook was created: ${url}`);
-
-bot.on("message", async (msg) => {
+const pingAll = (msg) => {
   const chatId = msg.chat.id;
-  try {
-    if (msg.text !== undefined) {
-      if (msg.text.includes("@all")) {
-        let message = "";
-        for (const id of ids) {
-          if (id !== msg.from.id) {
-            const member = await bot.getChatMember(chatId, id);
-            const name = member.user.first_name;
-            message += `[${name}](tg://user?id=${id}) `;
-          }
-        }
-        bot.sendMessage(chatId, message, { reply_to_message_id: msg.message_id, parse_mode: 'MarkdownV2' });
-      }
+  let message = "";
+  for (const id of IDS) {
+    if (id !== msg.from.id) {
+      const member = await bot.getChatMember(chatId, id);
+      const name = member.user.first_name;
+      message += `[${name}](tg://user?id=${id}) `;
     }
-  } catch (e) {
-    bot.sendMessage(chatId, `Something went wrong: ${e}`);
   }
-});
+  bot.sendMessage(chatId, message, { reply_to_message_id: msg.message_id, parse_mode: 'MarkdownV2' });
+}
+
+const setupBot = () => {
+  const bot = new TelegramBot(TOKEN, OPTIONS);
+  console.log("bot online");
+  const url = process.env.APP_URL || 'https://floating-falls-26181.herokuapp.com:443';;
+  bot.setWebHook(`${url}/bot${TOKEN}`);
+  console.log(`webhook was created: ${url}`);
+  
+  bot.on("message", async (msg) => {
+    try {
+      if (msg.text !== undefined) {
+        if (msg.text.includes("@all") || msg.text.includes("@all_42bot")) {
+          pingAll();
+        }
+      }
+    } catch (e) {
+      bot.sendMessage(msg.chat.id, `Something went wrong: ${e}`);
+    }
+  });
+}
+
+setupBot();
